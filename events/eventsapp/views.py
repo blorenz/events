6a8bb91725c07c1bc3519be63adf44fe8d986dfd
JCBEMaster <blorenz@gmail.com>
2012-03-22 22:33:39 -0400

    Latest

--- events/eventsapp/views.py
+++ events/eventsapp/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
-# Create your views here.
+from django import forms
+
+
+class CreateEventibleForm(forms.Form):
+    name = forms.CharField(max_length=100)    
+    
+    
+
+
 
 def home(request):
     c = RequestContext(request)
     return render_to_response('eventsapp/base.html',
                               {'entry_list': 'Hi Jon!!!'},
                            context_instance=c )
+
+def create(request):
+    c = RequestContext(request)
